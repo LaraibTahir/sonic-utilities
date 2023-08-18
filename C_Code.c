9f866314391847690f0007e674022964f1c4fa36
LaraibTahir <laraibtahir185@gmail.com>
2023-08-18 09:31:48 +0500

    About SONiC

---
+++ C_Code.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Display a message about SONiC
+    printf("SONiC (Software for Open Networking in the Cloud) is an open-source network operating system.\n");
+    printf("It provides a flexible and scalable platform for managing network switches and routers.\n");
+    printf("SONiC aims to promote open networking, interoperability, and innovation in networking technologies.\n");
+
+    return 0;
+}
